10fb2f4322ed38bc2343ee5def0883fc461aedc3
Siravit Tanratvijit <siravittanratvijit@Siravits-Air.ST-244F>
2023-05-29 00:58:54 +0700

    finish all-feature

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,7 +1,14 @@
 import Link from "next/link";
 import classes from "./button.module.css";
 
-export default function Button({ children, link }) {
+export default function Button({ children, link, onClick }) {
+  if (!link) {
+    return (
+      <button className={classes.btn} onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
   return (
     <Link className={classes.btn} href={link}>
       {children}
